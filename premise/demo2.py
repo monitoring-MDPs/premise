a9957978b5cdf3642bfc72e9e9e0733fdf8a6a0c
Luko van der Maas <luko.vandermaas@ru.nl>
2025-02-21 15:01:22 +0100

    Build state and transition list, and add to demo2.py. Also add Snakes and letter model

--- premise/demo2.py
+++ premise/demo2.py
@@ -2,19 +2,25 @@ from pathlib import Path
 import models
 import monitor
 import trace_generator
+import sys
+
+model_def = models.default_models[sys.argv[1] if len(sys.argv) > 1 else "SnL-10x10"]
 
 # Load the model into storm
 model, risk = models.build_model_and_risk(
-    models.ModelDescription(
-        Path(__file__).parent / "examples/airportA-7.nm",
-        "DMAX=5,PMAX=5",
-        'Pmax=? [F "crash"]',
-    ),
+    model_def,
     monitor.PremiseOptions(),
 )
 
+states, transtitions = models.build_state_and_transition_list(
+    model, model_def.target_label
+)
+print(states[:10], transtitions[:10])
+
 # Create the conditional trace generator and give it the target label
-ctr = trace_generator.ConditionalTraceGenerator(model, "crash")
+ctr = trace_generator.ConditionalTraceGenerator(
+    model, target_label=model_def.target_label
+)
 
 # Generate a random trace not conditioned on anything
 path, trace, bad = ctr.generate_random_trace([], 30)
@@ -29,8 +35,8 @@ print(bad)
 
 # Generate 1000 random traces of length 50 conditioned on the previous trace
 res = []
-for _ in range(1000):
-    print(".", end="")
+for _ in range(500):
+    print(".", end="", flush=True)
     res.append(ctr.generate_random_trace(trace, 50)[2] * 1)
 
 # Show the probability of reaching a bad state after the previous trace within 20 steps
