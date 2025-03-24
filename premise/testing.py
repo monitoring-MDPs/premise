36198339897d07f34e64d956101cfe9d925880f3
Luko van der Maas <luko.vandermaas@ru.nl>
2025-03-24 12:05:21 +0000

    Fix testing.py and new learned models

--- premise/testing.py
+++ premise/testing.py
@@ -1,9 +1,9 @@
 import argparse
-from numbers import Number
 from typing import Any
 import numpy as np
-import stormpy.pomdp
 import tqdm
+import matplotlib.pyplot as plt
+from sklearn import metrics
 
 from interval import create_monitor
 import models
@@ -29,6 +29,9 @@ if __name__ == "__main__":
         "-l", "--sample_length", type=int, help="Length of the samples to generate"
     )
     parser.add_argument(
+        "-s", "--samples", type=int, default=1000, help="Amount of samples to test on"
+    )
+    parser.add_argument(
         "--conformence-amount",
         type=int,
         default=1,
@@ -40,9 +43,9 @@ if __name__ == "__main__":
     )
     parser.add_argument("--verbose", "-v", action="count", default=0)
 
-    args = parser.parse_args() 
+    args = parser.parse_args()
 
-    #Get the real model definition
+    # Get the real model definition
     model_def = models.default_models[args.model_name]
 
     model_def.risk_property = f'Pmax=? [F<={args.horizon} "{model_def.target_label}" ]'
@@ -61,7 +64,7 @@ if __name__ == "__main__":
     interval = np.load(args.trans_path, allow_pickle=True)[()]
     initial_interval = np.load(args.init_path, allow_pickle=True)[()]
 
-    ctr = trace_generator.ConditionalTraceGenerator(  
+    ctr = trace_generator.ConditionalTraceGenerator(
         model, target_label=model_def.target_label
     )
 
@@ -77,85 +80,88 @@ if __name__ == "__main__":
         args.dump,
         args.verbose,
     )
-    
-    alarms = []
+
+    alarms: list[bool] = []
+    alarm_percent: list[float] = []
     risks = []
+    traces = []
 
-    testing_samples = []
+    for x in tqdm.trange(args.samples):
 
-    for x in range(10000): 
-        print(x)
+        trace = ctr.generate_random_trace([], args.sample_length)
+        traces.append(trace)
 
-    # Run premise on the learned model
-
-        sample = [ctr.generate_random_trace([], args.sample_length)]
-        testing_samples.append(sample)
-        risk: list[tuple[list, Any]] = []
-        for trace in tqdm.tqdm(sample):
-            mon.initialize(0)
-            observations = [t[1] for t in trace]
-            for obs in observations[:-1]:
-                mon.step(observation_map[obs], compute_risk=False)
-            last_risk = mon.step(observation_map[observations[-1]], compute_risk=True)
-            risk.append((trace, last_risk))
+        # Run premise on the learned model
+        mon.initialize(0)
+        observations = [s[1] for s in trace]
+        for obs in observations[:-1]:
+            mon.step(observation_map[obs], compute_risk=False)
+        last_risk = mon.step(observation_map[observations[-1]], compute_risk=True)
         risks.append(last_risk)
 
+        # Check correctness of the monitor
+        alarm = 0
+        for _ in range(args.conformence_amount):
+            new_trace = ctr.generate_random_trace(
+                [s[1] for s in trace], length=len(trace) + args.horizon
+            )
+            alarm += any([s[2] for s in new_trace])
+        alarms.append(alarm > 0)
+        alarm_percent.append(alarm / args.conformence_amount)
 
-    # Check correctness of the monitor 
-        res = []
-        for (trace, risk) in risk: 
-
-            alarm = 0
-            for _ in range(args.conformence_amount):
-                new_trace = ctr.generate_random_trace(
-                    [s[1] for s in trace], length=len(trace) + args.horizon
-                )
-                alarm += any([s[2] for s in new_trace])
-                alarms.append(alarm)
-                #if args.verbose > 0 and any([s[2] for s in new_trace]):
-                    #for s in new_trace:
-                        #if s[2]:
-                            #print("\033[92m", end="")
-                        #print(
-                            #model.state_valuations.get_string(s[0]).replace(" ", ""),
-                            #s[1],
-                            #end=" -> ",
-                        #)
-                        #if s[2]:
-                            #print("\033[0m", end="")
-                    #print()
-               
-            if args.verbose > 0:
-                input() 
-
+        if args.verbose > 0:
+            input()
 
     # Print statistics
     print("Results:")
 
-    not_reached = 0 
-    reached = 0 
-    false_negative = 0 
+    not_reached = 0
+    reached = 0
+    false_negative = 0
     false_positve = 0
 
+    np.save("/workspaces/premise/premise/examples/testing_samples.npy", trace)
+    np.save("/workspaces/premise/premise/examples/risk_model_based.npy", risks)
+    np.save("/workspaces/premise/premise/examples/alarm_percent.npy", alarm_percent)
+    np.save("/workspaces/premise/premise/examples/alarms.npy", alarms)
 
-    for x in range(10000):
-        if alarms[x] == 0:
-            not_reached += 1 
-            if (risks[x] > (0.2)): #0.14
+    for x in range(args.samples):
+        if not alarms[x]:
+            not_reached += 1
+            if risks[x] > (0.03):
                 false_positve += 1
-        if alarms[x] == 1:
-            reached += 1 
-            if (risks[x] < (0.2)): #0.14
-                false_negative +=1 
-    
+        if alarms[x]:
+            reached += 1
+            if risks[x] < (0.03):
+                false_negative += 1
 
-    print(f"Not reached : {not_reached/10000}")
-    print(f"Reached : {reached/10000}")
+    print(f"Not reached : {not_reached/args.samples}")
+    print(f"Reached : {reached/args.samples}")
     print(f"False Positives: {false_positve/not_reached}")
     print(f"False Negatives: {false_negative/reached}")
-    print(f"Error rate: {(false_negative + false_positve)/ 10000}")
- 
-    np.save('/workspaces/premise/premise/examples/testing_samples.npy', testing_samples)
-    np.save('/workspaces/premise/premise/examples/risk_model_based.npy', risks)
-    np.save('/workspaces/premise/premise/examples/alarms.npy', alarms)
- 
+    print(f"Error rate: {(false_negative + false_positve)/ args.samples}")
+
+    fpr, tpr, threshold = metrics.roc_curve(alarms, risks)
+    roc_auc = metrics.auc(fpr, tpr)
+
+    fig, axis = plt.subplots(1, 2, figsize=(8, 6))
+
+    axis[0].set_title("Receiver Operating Characteristic")
+    axis[0].plot(fpr, tpr, "b", label="AUC = %0.2f" % roc_auc)
+    axis[0].legend(loc="lower right")
+    axis[0].plot([0, 1], [0, 1], "r--")
+    axis[0].set_xlim([0, 1])
+    axis[0].set_ylim([0, 1])
+    axis[0].set_ylabel("True Positive Rate")
+    axis[0].set_xlabel("False Positive Rate")
+
+    axis[1].set_title("True alarm rate against learned premise alarm rate")
+    axis[1].scatter(risks, alarm_percent)
+    axis[1].plot([0, 1], [0, 1], "r--")
+    axis[1].set_xlabel("learned risk")
+    axis[1].set_ylabel("True alarm rate")
+    axis[1].set_xlim([0, 1])
+    axis[1].set_ylim([0, 1])
+
+    fig.set_size_inches(18.5, 10.5)
+    plt.savefig("res.png")
