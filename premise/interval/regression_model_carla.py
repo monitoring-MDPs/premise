945038f571580d4c00c8877ccb5de0ddeb44d04d
Antonina Skurka <skurka@chalmers.se>
2025-05-08 08:25:21 +0000

    container

--- premise/interval/regression_model_carla.py
+++ premise/interval/regression_model_carla.py
@@ -5,11 +5,36 @@ import pickle
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-#leaning_samples = np.load("/workspaces/premise/premise/examples/learning_samples.npy")
 
-with open('/workspaces/premise/premise/carla/carla_samples/all_data.pkl', "rb") as f:
+with open('/workspaces/premise/premise/carla/carla_samples/all_data_more.pkl', "rb") as f:
     learning_samples = pickle.load(f)
 
+
+with open('/workspaces/premise/premise/carla/carla_samples/all_data.pkl', "rb") as f:
+    base_data = pickle.load(f)
+
+with open('/workspaces/premise/premise/carla/carla_samples/HHH_0_50.pkl', "rb") as f:
+    HHH_data_1 = pickle.load(f)
+
+with open('/workspaces/premise/premise/carla/carla_samples/HHH_0_50.pkl', "rb") as f:
+    HHH_data_2 = pickle.load(f)
+
+with open('/workspaces/premise/premise/carla/carla_samples/HHH_0_50.pkl', "rb") as f:
+    HHH_data_3 = pickle.load(f)
+
+with open('/workspaces/premise/premise/carla/carla_samples/HHH_0_50.pkl', "rb") as f:
+    HHH_data_4 = pickle.load(f)
+
+with open('/workspaces/premise/premise/carla/carla_samples/HHH_0_50.pkl', "rb") as f:
+    HHH_data_5 = pickle.load(f)
+
+
+#learning_samples = base_data
+#learning_samples = base_data +  HHH_data_1
+#learning_samples = base_data +  HHH_data_1 + HHH_data_2 
+#learning_samples = base_data +  HHH_data_1 + HHH_data_2 + HHH_data_3 
+#learning_samples = base_data +  HHH_data_1 + HHH_data_2 + HHH_data_3 + HHH_data_4 
+learning_samples = base_data +  HHH_data_1 + HHH_data_2 + HHH_data_3 + HHH_data_4 + HHH_data_5
 
 X = []
 y = []
@@ -18,18 +43,18 @@ for l in learning_samples:  # 1000 traces
     #for s in learning_samples[l]:  # s - state
         # Flatten the trace and append to training_data
         flattened_trace = []
-        for x in l[:-225]:  # Exclude the last 200 states (the monitor is given the first 50)
+        for x in l[:-230]:  # Exclude the last x states (the monitor is given the first (250-x)
             flattened_trace.append(x[1])  # Assuming x[1] contains the feature value (observation)
         X.append(flattened_trace)
 
         # Determine the prediction based on the last 5 elements in the trace
-        if any(x[2] == True for x in l[-225:-150]): # horizon of 75
+        if any(x[2] == True for x in l[-230:-180]): # horizon 
             y.append(1)
         else:
             y.append(0)
     
 
-num_steps = 25
+num_steps = 20
 
 
 colors = ['LLL', 'LLM', 'LLH', 'LML', 'LMM', 'LMH', 'LHL', 'LHM', 'LHH',
@@ -60,7 +85,7 @@ model = LogisticRegression()
 model.fit(X, y)
 
 #testing_samples = np.load("/workspaces/premise/premise/examples/testing_samples.npy")
-X_test =  [[('HHH', 'pd60')] * 25]
+X_test =  [[('HHH', 'pd60')] * 20]
 
 binary_test_data = []
 for trace in X_test:
@@ -89,3 +114,15 @@ print(prob_class_1)
 #risk_model_based = np.load("/workspaces/premise/premise/examples/risk_model_based.npy")
 
 
+regression_risks = {}
+
+regression_risks['HHH,pd60,20,80'] = [0.03390136,0.03474597,0.03554655,0.03636899,0.03701464,0.03798529]
+regression_risks['HHH,pd60,20,50'] = [0.00021963, 0.00023484, 0.00024855, 0.0002198, 0.00021432, 0.00019886]
+
+for r in regression_risks.keys(): 
+    print(r, regression_risks)
+
+
+
+
+print(regression_risks)
