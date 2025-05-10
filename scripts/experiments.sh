4858a2f7d7159b9a2558211711ab14eeb177756d
Luko van der Maas <luko.vandermaas@ru.nl>
2025-05-10 12:11:11 +0200

    small refine fix and better lengths for commands

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -2,8 +2,8 @@
 set -x
 
 now=$(date +"%Y-%m-%d_%H-%M-%S")
-mkdir -p out/logs
+mkdir -p out/logs/$now
 mkdir -p out/stats/$now
 mkdir -p out/models/$now
 
-sed "s/date/${now}/g" scripts/commands.sh | sed '/^#/d' | parallel --verbose --results out/logs/$now/{#}_{}/ --ungroup --eta --no-run-if-empty
+sed "s/date/${now}/g" scripts/commands.sh | sed '/^#/d' | parallel --verbose --results out/logs/$now/{#}_{}/ --ungroup --eta --no-run-if-empty --joblog out/logs/$now/joblog.tsv
