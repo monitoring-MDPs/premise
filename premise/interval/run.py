ad8632985c0f175e108ffd3ce0e56ef08ad546ad
Luko van der Maas <luko.vandermaas@ru.nl>
2025-05-12 13:52:02 +0200

    add timeout to run

--- premise/interval/run.py
+++ premise/interval/run.py
@@ -1,4 +1,6 @@
+import signal
 import sys
+from typing import NoReturn
 
 from premise.interval.regression_model import reg_argsparser, reg_main
 from premise.interval.refinement import ref_args_parser, ref_main
@@ -14,13 +16,37 @@ def split_args(args, delim):
     return res
 
 
+def timeout_handler(signum, frame) -> NoReturn:
+    print("Timeout occurred")
+    exit(2)
+    raise Exception("Timeout occurred")
+
+
 if __name__ == "__main__":
-    args = split_args(sys.argv[2:], "::")
-    if sys.argv[1] == "refinement":
+    args = split_args(sys.argv[3:], "::")
+
+    # Calculate timeout
+    if sys.argv[1][-1] == "h":
+        timeout = int(sys.argv[1][:-1]) * 60 * 60
+    elif sys.argv[1][-1] == "m":
+        timeout = int(sys.argv[1][:-1]) * 60
+    elif sys.argv[1][-1] == "s":
+        timeout = int(sys.argv[1][:-1])
+
+    print("Timeout: ", timeout)
+
+    if sys.argv[2] == "refinement":
         ref_parser = ref_args_parser()
         ref_args = ref_parser.parse_args(args[0])
-        ref_main(ref_args)
-    elif sys.argv[1] == "comp_methods":
+        signal.signal(signal.SIGALRM, timeout_handler)
+        signal.alarm(timeout)
+        try:
+            ref_main(ref_args)
+        except TimeoutError:
+            print("Refinement timed out.")
+        finally:
+            signal.alarm(0)
+    elif sys.argv[2] == "comp_methods":
         if len(args) != 3:
             print(
                 "Usage: python run.py comp_methods <args refinement> <> <args no refinement without -ss and -sc> <> <args regression>",
@@ -31,7 +57,18 @@ if __name__ == "__main__":
         ref_parser = ref_args_parser()
 
         ref_args = ref_parser.parse_args(args[0])
-        ref_stats = ref_main(ref_args)
+
+        signal.signal(signal.SIGALRM, timeout_handler)
+        print("Starting timeout", timeout)
+        try:
+            signal.alarm(timeout)
+            ref_stats = ref_main(ref_args)
+        except TimeoutError:
+            print("Refinement timed out, stopping experiment.")
+            exit(1)
+        finally:
+            signal.alarm(0)
+
         samples = ref_stats["sample_count"]
 
         print("Samples from refinement: ", samples)
@@ -39,9 +76,21 @@ if __name__ == "__main__":
         ref_args_2 = ref_parser.parse_args(args[1])
         ref_args_2.stopping_samples = samples
         ref_args_2.stopping_criteria = "samples"
-        ref_main(ref_args_2)
+        signal.alarm(timeout)
+        try:
+            ref_main(ref_args_2)
+        except TimeoutError:
+            print("No-refinement timed out, continue to regression.")
+        finally:
+            signal.alarm(0)
 
         reg_parser = reg_argsparser()
         reg_args = reg_parser.parse_args(args[2])
         reg_args.amount = samples
-        reg_main(reg_args)
+        signal.alarm(timeout)
+        try:
+            reg_main(reg_args)
+        except TimeoutError:
+            print("Regression timed out.")
+        finally:
+            signal.alarm(0)
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,17 +3,17 @@
 # The string "date" will be replaced by the current date and time
 
 # Normal refinement
-# timeout -v 4h python -u -m premise.interval.refinement -mc airportA-7-10-10 -ll 100 -ia 100 -ra 2 -d mse -ca 50 -cl 20 -ho 10 -sc threshold -sp 3 --dump-stats out/stats/date/airportA-7-10-10_refinement.npy -v --epsilon 0.005 -t -m out/models/date/airportA-7-10-10
-# timeout -v 4h python -u -m premise.interval.refinement -mc airportA-7-50-30 -ll 200 -ia 100 -ra 2 -d mse -ca 50 -cl 75 -ho 20 -sc threshold -sp 3 --dump-stats out/stats/date/airportA-7-50-30_refinement.npy -v --epsilon 0.0005 -t -m out/models/date/airportA-7-50-30
-# timeout -v 4h python -u -m premise.interval.refinement -mc airportB-7-50-30 -ll 200 -ia 100 -ra 2 -d mse -ca 50 -cl 75 -ho 20 -sc threshold -sp 3 --dump-stats out/stats/date/airportB-7-50-30_refinement.npy -v --epsilon 0.0005 -t -m out/models/date/airportB-7-50-30
-# timeout -v 4h python -u -m premise.interval.refinement -mc SnL-10x10 -ll 20 -ia 100 -ra 2 -t -ca 50 -cl 15 -ho 5 -sc threshold -sp 3 --dump-stats out/stats/date/SnL-10x10_refinement.npy -v -m out/models/date/SnL-10x10
-# timeout -v 4h python -m premise.interval.refinement -mc evadeV-6-3 -ll 60 -ia 100 -ra 2 -t -ca 50 -cl 40 -ho 12 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/evadev.npy -m out/models/date/evadeV -v
-# timeout -v 4h python -m premise.interval.refinement -mc evadeI-15 -ll 60 -ia 100 -ra 2 -t -ca 50 -cl 40 -ho 12 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/evadei.npy -m out/models/date/evadeI -v
+# python -u -m premise.interval.refinement -mc airportA-7-10-10 -ll 100 -ia 100 -ra 2 -d mse -ca 50 -cl 20 -ho 10 -sc threshold -sp 3 --dump-stats out/stats/date/airportA-7-10-10_refinement.npy -v --epsilon 0.005 -t -m out/models/date/airportA-7-10-10
+# python -u -m premise.interval.refinement -mc airportA-7-50-30 -ll 200 -ia 100 -ra 2 -d mse -ca 50 -cl 75 -ho 20 -sc threshold -sp 3 --dump-stats out/stats/date/airportA-7-50-30_refinement.npy -v --epsilon 0.0005 -t -m out/models/date/airportA-7-50-30
+# python -u -m premise.interval.refinement -mc airportB-7-50-30 -ll 200 -ia 100 -ra 2 -d mse -ca 50 -cl 75 -ho 20 -sc threshold -sp 3 --dump-stats out/stats/date/airportB-7-50-30_refinement.npy -v --epsilon 0.0005 -t -m out/models/date/airportB-7-50-30
+# python -u -m premise.interval.refinement -mc SnL-10x10 -ll 20 -ia 100 -ra 2 -t -ca 50 -cl 15 -ho 5 -sc threshold -sp 3 --dump-stats out/stats/date/SnL-10x10_refinement.npy -v -m out/models/date/SnL-10x10
+# python -m premise.interval.refinement -mc evadeV-6-3 -ll 60 -ia 100 -ra 2 -t -ca 50 -cl 40 -ho 12 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/evadev.npy -m out/models/date/evadeV -v
+# python -m premise.interval.refinement -mc evadeI-15 -ll 60 -ia 100 -ra 2 -t -ca 50 -cl 40 -ho 12 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/evadei.npy -m out/models/date/evadeI -v
 
 # # Coarse models
-# timeout -v 4h python -m premise.interval.refinement -mc SnLw-10x10 -sv pos -ll 20 -ia 100 -ra 2 -t -ca 50 -cl 15 -ho 5 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/SnL-coarse.npy -m out/models/date/SnLC -v
-# timeout -v 4h python -m premise.interval.refinement -mc evadeV-6-3-coarse -sv start turn c_ax c_ay c_dx c_dy -ll 60 -ia 100 -ra 2 -t -ca 50 -cl 40 -ho 12 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/evadevcoarse.npy -m out/models/date/evadeVC -v
-# timeout -v 4h python -u -m premise.interval.refinement -mc airportB-7-50-30 -sv d p pobs turn -ll 200 -ia 100 -ra 2 -d mse -ca 50 -cl 75 -ho 20 -sc threshold -sp 3 --dump-stats out/stats/date/airportB-7-50-30-coarse_refinement.npy -v --epsilon 0.0005 -t -m out/models/date/airportB-7-50-30-coarse
+# python -m premise.interval.refinement -mc SnLw-10x10 -sv pos -ll 20 -ia 100 -ra 2 -t -ca 50 -cl 15 -ho 5 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/SnL-coarse.npy -m out/models/date/SnLC -v
+# python -m premise.interval.refinement -mc evadeV-6-3-coarse -sv start turn c_ax c_ay c_dx c_dy -ll 60 -ia 100 -ra 2 -t -ca 50 -cl 40 -ho 12 -sc threshold -sp 3 -st 0.01 --dump-stats out/stats/date/evadevcoarse.npy -m out/models/date/evadeVC -v
+# python -u -m premise.interval.refinement -mc airportB-7-50-30 -sv d p pobs turn -ll 200 -ia 100 -ra 2 -d mse -ca 50 -cl 75 -ho 20 -sc threshold -sp 3 --dump-stats out/stats/date/airportB-7-50-30-coarse_refinement.npy -v --epsilon 0.0005 -t -m out/models/date/airportB-7-50-30-coarse
 
 
 # Compare methods
