07d96bc02cfc89c5c0296642d25f18645fa96dff
Antonina Skurka <skurka@chalmers.se>
2025-05-20 11:21:00 +0000

    test exploration

---
+++ premise/interval/regression_model_exploration.py
@@ -0,0 +1,382 @@
+import numpy as np
+import pandas as pd
+from typing import Any
+from sklearn.metrics import roc_curve, auc
+
+from premise.monitor import Monitor
+from sklearn.linear_model import LogisticRegression
+
+import tqdm
+import argparse
+import matplotlib.pyplot as plt
+from sklearn import metrics
+
+from premise.interval.loading import build_suo, build_suo_args_parser
+from premise.interval.loss import distance_measures
+from premise.interval.interval import Samples, Trace, create_monitor
+from premise.interval.learningIMC import learn_IMC
+from premise.interval.conformence import test_monitor
+
+
+#def learn_model_based(all_states, all_transitions, initial_states, train_samples, testing_samples): 
+#    min_width = 0.0
+#    epsilon = 0.0001
+#    initial_interval, interval = learn_IMC(all_states, all_transitions, initial_states, train_samples, min_width, epsilon)
+
+
+    #Build the premise monitor on the learned model
+#    mon, observation_map, unfolder, ipomdp = create_monitor(
+#        interval,
+#        initial_interval,
+#        "min",
+#        True,
+#        args.horizon,
+#        dump_path=args.dump_model + "monitor" if args.dump_model else None,
+#        verbose=args.verbose,
+#    ) 
+  
+#    IMC_risks = test_monitor(
+#        mon,
+#        testing_samples,
+#        obs_func=lambda x: observation_map[x],
+#        skip_initial=True,
+#    )
+#
+#    return IMC_risks
+
+
+def learn_regression_model(train_samples, observations, testing_samples, args):
+    X = []
+    y = []
+
+    for l in train_samples:  # path
+        flattened_trace = []
+        for x in l[: -args.horizon]:  # Exclude the horizon length
+            flattened_trace.append(
+                x[1]
+            )  # Assuming x[1] contains the observable variables
+        X.append(flattened_trace)
+
+        # Determine the prediction based on the horizon
+        if any(
+            x[2] == True for x in l[-args.horizon:]
+            
+        ):  # Look at the last h steps for error state #CHECK THE LABEL NAMES
+            y.append(1)
+        else:
+            y.append(0)
+
+    num_steps = args.length
+
+    column_names = [f"Step{s}_Obs{o}" for s in range(num_steps) for o in observations]
+
+    binary_data = []
+    for trace in X:
+        row = []
+        for step in range(num_steps):
+            obs = trace[step]
+            row.extend([1 if obs == o else 0 for o in observations])
+        binary_data.append(row)
+
+    X = pd.DataFrame(binary_data, columns=column_names)
+
+    model = LogisticRegression()
+    model.fit(X, y)
+
+    testing_traces = []
+    alarms = {}
+
+    for s in testing_samples:
+        t = tuple(x[1] for x in s[:-args.horizon])
+        testing_traces.append(t)
+
+    for s in testing_samples:
+        alarms[tuple(s)] = any([x[2] for x in s[-args.horizon:]])
+        
+    binary_test_data = []
+    for trace in testing_traces:
+        row = []
+        for step in range(num_steps):
+            obs = trace[step]
+            row.extend([1 if obs == o else 0 for o in observations])
+        binary_test_data.append(row)
+
+    X_test = pd.DataFrame(binary_test_data, columns=column_names)
+
+    prob = model.predict_proba(X_test)
+
+    risks = prob[:, 1]
+
+    regression_risks = {}
+    for sample, risk in zip(testing_samples, risks):
+        regression_risks[tuple(sample)] = risk
+
+    return testing_traces, alarms, regression_risks, model # dictionary trace + risk
+
+
+def test_regression_monitor(
+    mon: Monitor,
+    samples: Samples,
+    obs_func=lambda x: x,
+    skip_initial=False,
+    with_tqdm=True,
+):
+    risks: dict[Trace, Any] = {}
+    it = tqdm.tqdm(samples) if with_tqdm else samples
+    for trace in it:
+        observable = [t[1] for t in trace]
+
+        if skip_initial:
+            mon.initialize(0)
+        else:
+            mon.initialize(obs_func(observable[0]))
+
+        for obser in observable[0 if skip_initial else 1 : -1]:
+            mon.step(obs_func(obser), compute_risk=False)
+
+        last_risk = mon.step(obs_func(observable[-1]), compute_risk=True)
+        risks[tuple(trace)] = last_risk
+    return risks
+
+
+#def measure_distance(testing_samples, regression_risks, IMC_risks, distance, test_weights, suo, args):   
+def measure_distance(testing_samples, regression_risks, distance, test_weights, suo, args):   
+
+    target_risks = test_regression_monitor(
+        suo.create_target_monitor(),
+        testing_samples,
+    )
+
+    target_dist_reg, target_all_dist_reg = distance.distance(
+        test_weights,
+        {s: float(r) for s, r in target_risks.items()},
+        {s: float(r) for s, r in regression_risks.items()},
+        all_distances=True,
+    )
+
+    #target_dist_imc, target_all_dist_imc = distance.distance(
+    #    test_weights,
+    #    {s: float(r) for s, r in target_risks.items()},
+    #    {s: float(r) for s, r in IMC_risks.items()},
+    #    all_distances=True,
+    #)
+
+    print(f"Regression's distance to target risk: {target_dist_reg}")
+    #print(f"IMC-based distance to target risk: {target_dist_imc}")
+
+
+    #return target_risks, target_dist_reg, target_all_dist_reg, target_dist_imc, target_all_dist_imc
+    return target_risks, target_dist_reg, target_all_dist_reg
+
+def plot_roc_curve(alarms, risks, fname=None):
+    alarm_values = list(alarms.values())
+    risk_values = list(risks.values())
+
+    fpr, tpr, _ = roc_curve(alarm_values, risk_values)
+    auc_value = auc(fpr, tpr)
+
+    print(f"AUC: {auc_value:.4f}")
+
+
+    #fpr, tpr, threshold = metrics.roc_curve(alarms, risks)
+    #roc_auc = metrics.auc(fpr, tpr)
+
+    #plt.figure(figsize=(8, 6))
+    #plt.title("Receiver Operating Characteristic")
+    #plt.plot(fpr, tpr, "b", label="AUC = %0.2f" % roc_auc)
+    #plt.plot([0, 1], [0, 1], "r--")
+    #plt.xlim((0, 1))
+    #plt.ylim((0, 1))
+    #plt.ylabel("True Positive Rate")
+    #plt.xlabel("False Positive Rate")
+    #plt.legend(loc="lower right")
+    #plt.grid(True)
+    #if fname:
+    #    plt.savefig(fname)
+    #plt.show()
+    #print(f"AUC: {roc_auc:.4f}")
+    return auc_value
+
+#def monitoring_analysis(alarms, target_risks, regression_risks, IMC_risks): 
+def monitoring_analysis(alarms, target_risks, regression_risks): 
+
+    error = 0 
+    for x in alarms.keys(): 
+        if alarms[x] == True: 
+            error += 1
+
+    print(f"Error rate in samples: {error/len(alarms)}")
+    
+    print("Regression monitor")
+    auc_value_reg = plot_roc_curve(alarms, regression_risks)
+
+    #print("IMC monitor")
+    #auc_value_imc = plot_roc_curve(alarms, IMC_risks)
+
+    print("Target monitor")
+    auc_value_target = plot_roc_curve(alarms, target_risks)
+
+    #return auc_value_reg, auc_value_imc, auc_value_target
+    return auc_value_reg, auc_value_target
+
+
+
+def reg_main(args: argparse.Namespace):
+    print(f"Learned on {args.amount} samples") 
+
+    suo = build_suo(args)
+
+    train_samples = suo.generate_random_traces(
+        [], args.length + args.horizon, args.amount
+    )
+
+    #all_states, all_transitions, initial_states = suo.get_states_and_transitions(
+    #    all_transitions=not args.existing_transitions
+    #)
+
+    all_states, all_transitions, initial_states = suo.get_states_and_transitions() #all possible transitions
+
+    observations = []
+    for x in all_states:
+        if x[1] not in observations:
+            observations.append(x[1])
+    
+
+    samples_with_prob = suo.generate_random_traces_with_prob(
+        [], args.length + args.horizon, args.test_samples
+    )
+
+    total_prob = sum(p for _, p in samples_with_prob)
+    test_weights = {s: float(p / total_prob) for s, p in samples_with_prob}
+    testing_samples = [s[0] for s in samples_with_prob]
+
+    distance = distance_measures[args.distance]()
+
+    #IMC_risks = learn_model_based(
+    #    all_states, all_transitions, initial_states, train_samples, testing_samples)
+
+    testing_traces, alarms, regression_risks, model = learn_regression_model(
+        train_samples, observations, testing_samples, args
+    )
+
+
+
+    #target_risks, target_dist_reg, target_all_dist_reg, target_dist_imc, target_all_dist_imc = measure_distance(
+    #    testing_samples, regression_risks, IMC_risks, distance, test_weights, suo, args) 
+
+    target_risks, target_dist_reg, target_all_dist_reg = measure_distance(
+        testing_samples, regression_risks, distance, test_weights, suo, args) 
+
+
+
+    #auc_value_reg, auc_value_imc, auc_value_target = monitoring_analysis(alarms, target_risks, regression_risks, IMC_risks)
+    auc_value_reg, auc_value_target = monitoring_analysis(alarms, target_risks, regression_risks)
+
+
+
+    if args.dump_stats:
+        np.save(
+            args.dump_stats,
+            {
+            "target_dist": target_dist,
+            "target_all_dist": target_all_dist,
+            "weights": {s: float(w) for s, w in test_weights.items()},
+            "target_risks": {s: float(r) for s, r in target_risks.items()},
+            "regression_risks": {s: float(r) for s, r in regression_risks.items()},
+            "samples": testing_samples,
+            "args": vars(args),
+            "observations": observations
+            },  # type: ignore
+        )
+
+    if args.model_path: 
+        np.save(args.model_path, model)
+    
+    #return target_dist_reg, target_dist_imc, auc_value_reg, auc_value_imc, auc_value_target
+    return target_dist_reg, auc_value_reg, auc_value_target
+
+
+
+def build_learning_args_parser(parser: argparse.ArgumentParser):
+    group = parser.add_argument_group("Learning Parameters")
+    group.add_argument(
+        "-m", "--model-path", type=str, default=None, help="Path to store the model"
+    )
+    group.add_argument(
+        "-a", "--amount", type=int, default=1000, help="Amount of samples to generate"
+    )
+    group.add_argument(
+        "-l", "--length", type=int, default=15, help="Length of the samples to generate"
+    )
+    group.add_argument(
+        "-t", "--test_samples", type=int, default=500, help="Amount of test samples"
+    )
+    group.add_argument(
+        "--model", type=bool, default=False, help="If a model exists"
+    )
+    group.add_argument("--horizon", type=int, default=5, help="Length horizon")
+
+
+def reg_argsparser():
+    parser = argparse.ArgumentParser(description="Learn an IMC")
+    build_suo_args_parser(parser)
+    build_learning_args_parser(parser)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase verbosity level (can be used multiple times)",
+    )
+    parser.add_argument(
+        "-d",
+        "--distance",
+        choices=distance_measures.keys(),
+        default="mae",
+        help="Distance measure to use",
+    )
+
+    parser.add_argument("--dump-model", type=str, help="Path to dump the model to")
+    parser.add_argument(
+        "--dump-stats", type=str, help="Path to the file to dump stats to"
+    )
+
+    return parser
+
+if __name__ == "__main__":
+    parser = reg_argsparser()
+    args = parser.parse_args()
+
+    model_free_distance = []
+    #model_based_distance = []
+    model_free_AUC = []
+    #model_based_AUC = []
+    target_AUC = []
+
+    for x in range (250, 10250, 250):
+        args.amount = x
+        #target_dist_reg, target_dist_imc, auc_value_reg, auc_value_imc, auc_value_target  = reg_main(args)
+        target_dist_reg, auc_value_reg, auc_value_target  = reg_main(args)
+
+        model_free_distance.append(target_dist_reg)
+        #model_based_distance.append(target_dist_imc)
+
+        model_free_AUC.append(auc_value_reg)
+        #model_based_AUC.append(auc_value_imc)
+        target_AUC.append(auc_value_target)
+
+        args = parser.parse_args()
+        suo = build_suo(args)
+    
+    print(model_free_distance)
+    #print(model_based_distance)
+    print(target_AUC)
+    print(model_free_AUC)
+    #print(model_based_AUC)
+
+
+
+    
+
+
+
