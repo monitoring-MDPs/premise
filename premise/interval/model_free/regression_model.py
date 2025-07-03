1d6c572cc336d4ab000e1b003fadae95d3c5b1ec
Antonina Skurka <skurka@chalmers.se>
2025-07-03 08:36:47 +0000

    regression model - labeling data

--- premise/interval/model_free/regression_model.py
+++ premise/interval/model_free/regression_model.py
@@ -32,7 +32,7 @@ def learn_regression_model(train_samples, observations, testing_samples, args):
         row = prep_trace_for_regression(sub_trace, observations)
         binary_data.append(row)
 
-        y.append(1 if any(x[2] == True for x in sub_trace) else 0)
+        y.append(1 if any(x[2] == 1 for x in sub_trace) else 0)
 
     X = pd.DataFrame(binary_data, columns=column_names)
 
